c3439ecf7f2f6367c10850364defb84e28cb9374
aahad91 <abdulahadayaz@hotmail.com>
2019-08-01 03:49:21 +0200

    updated script.sh and docker file

--- squid_client/script.sh
+++ squid_client/script.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-ab -n 5000 -c 10 -X 172.20.0.2:3128 http://www.google.com/ > test1.txt
+curl -x 172.20.0.2:3128 -L www.google.com  &&
+ab -n 5000 -c 10 -X 172.20.0.2:3128 http://www.google.com/ > test1.txt &&
+curl -x 172.20.0.2:3128 -L www.facebook.com  &&
 ab -n 5000 -c 5 -X 172.20.0.2:3128 http://www.facebook.com/ > test2.txt
+exec "$@";
