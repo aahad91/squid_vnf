ad10142cf4433b4d538dd4e7bd41be4f059f590f
Abdul Ahad Ayaz <49203564+aahad91@users.noreply.github.com>
2019-11-26 14:23:28 +0100

    test script for travis

---
+++ test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+virt-customize -a xenial-server-cloudimg-amd64-disk1.img \
+      --run-command 'apt-get update' \
+      --run-command 'DEBIAN_FRONTEND=noninteractive' \
+      --run-command 'apt-get install -y \
+                      net-tools \
+                      iproute2 \
+                      inetutils-ping \
+                      iptables \
+                      squid3 \
+                      curl \
+                      python \
+                      python-yaml \
+                      python3 \
+                      python3-pip'
+      --run-command 'cd /' \
+      --upload /image-descriptors/squid/squid.conf:/etc/squid/squid.conf \
+      --upload /image-descriptors/squid/start.sh:/start.sh \
+      --upload /image-descriptors/squid/stop.sh:/stop.sh \
+      --run-command 'chmod +x start.sh' \
+      --run-command 'chmod +x stop.sh' \
+      --mkdir /tngbench_share
