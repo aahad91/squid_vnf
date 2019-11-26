dd755df2f42633878c6bdc17b54d4772bdaca7f3
Abdul Ahad Ayaz <49203564+aahad91@users.noreply.github.com>
2019-11-26 14:57:38 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-virt-customize -a xenial-server-cloudimg-amd64-disk1.img \
+virt-customize -v -a xenial-server-cloudimg-amd64-disk1.img \
       --run-command 'apt-get update' \
       --run-command 'DEBIAN_FRONTEND=noninteractive' \
       --run-command 'apt-get install -y \
@@ -14,7 +14,7 @@ virt-customize -a xenial-server-cloudimg-amd64-disk1.img \
                       python3 \
                       python3-pip' \
       --run-command 'cd /' \
-      --upload /image-descriptors/squid/squid.conf:/etc/squid/squid.conf \
+      --upload image-descriptors/squid/squid.conf:/etc/squid/squid.conf \
       --upload /image-descriptors/squid/start.sh:/start.sh \
       --upload /image-descriptors/squid/stop.sh:/stop.sh \
       --run-command 'chmod +x start.sh' \
