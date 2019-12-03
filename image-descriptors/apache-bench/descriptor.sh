78edc66e34eb9fcc0f609198c51cba6bb01969dd
aahad91 <abdulahadayaz@hotmail.com>
2019-12-03 15:59:13 +0100

    script files for image creation

--- image-descriptors/apache-bench/descriptor.sh
+++ image-descriptors/apache-bench/descriptor.sh
@@ -1,8 +1,10 @@
-# Set of packages and commands to build squid image
+#!/bin/bash
+
+# Set of packages and commands to build apache-bench image
 #Step1:
-sudo apt update
-sudo apt-get -y install libguestfs-tools
-export LIBGUESTFS_BACKEND=direct
+#sudo apt update
+#sudo apt-get -y install libguestfs-tools
+#export LIBGUESTFS_BACKEND=direct
 
 #Step2:
 wget http://cloud-images.ubuntu.com/xenial/current/xenial-server-cloudimg-amd64-disk1.img
@@ -12,23 +14,34 @@ wget http://cloud-images.ubuntu.com/xenial/current/xenial-server-cloudimg-amd64-
 # syntax: virt-customize -a [DOWNLOADED_FILE_NAME] --install [LIST_OF_PACKAGES]
 #         virt-customize -a [DOWNLOADED_FILE_NAME] --run-command 'apt-get update'
 #         virt-customize -a [DOWNLOADED_FILE_NAME] --mkdir [folder_name]
-          virt-customize -a [DOWNLOADED_FILE_NAME] --upload [src-file:dest-file]
-sudo virt-customize -a xenial-server-cloudimg-amd64-disk1.img \
+#         virt-customize -a [DOWNLOADED_FILE_NAME] --upload [src-file:dest-file]
+
+#Run with sudo
+virt-customize -a xenial-server-cloudimg-amd64-disk1.img \
       --run-command 'apt-get update' \
       --run-command 'DEBIAN_FRONTEND=noninteractive' \
       --run-command 'apt-get install -y \
+                      software-properties-common \
                       net-tools \
                       iproute2 \
                       inetutils-ping \
                       iptables \
-                      squid3 \
+                      aptitude \
+                      wget \
                       curl \
+                      iperf3 \
+                  		iperf \
+                      siege \
+                  		httperf \
+                      hping3 \
+                  		polygraph \
+                      apache2-utils \
                       python \
                       python-yaml \
                       python3 \
                       python3-pip' \
+      --run-command 'useradd -s /bin/bash -d /home/ubuntu -m ubuntu'\
       --run-command 'cd /home/ubuntu' \
-      --upload squid.conf:/etc/squid/squid.conf \
       --upload start.sh:/start.sh \
       --upload stop.sh:/stop.sh \
       --run-command 'chown ubuntu start.sh' \
@@ -37,5 +50,5 @@ sudo virt-customize -a xenial-server-cloudimg-amd64-disk1.img \
       --run-command 'chmod +x stop.sh' \
       --mkdir /tngbench_share
 
-# OpenStack command to add squid image
-openstack image create --public --disk-format qcow2 --container-format bare --file xenial-server-cloudimg-amd64-disk1.img squid-vm
+#OpenStack command
+#openstack image create --public --disk-format qcow2 --container-format bare --file xenial-server-cloudimg-amd64-disk1.img apache-bench-ubuntu
--- image-descriptors/squid/descriptor.sh
+++ image-descriptors/squid/descriptor.sh
@@ -1,8 +1,10 @@
-# Set of packages and commands to build apache-bench image
+#!/bin/bash
+
+# Set of packages and commands to build squid image
 #Step1:
-sudo apt update
-sudo apt-get -y install libguestfs-tools
-export LIBGUESTFS_BACKEND=direct
+#sudo apt update
+#sudo apt-get -y install libguestfs-tools
+#export LIBGUESTFS_BACKEND=direct
 
 #Step2:
 wget http://cloud-images.ubuntu.com/xenial/current/xenial-server-cloudimg-amd64-disk1.img
@@ -13,32 +15,24 @@ wget http://cloud-images.ubuntu.com/xenial/current/xenial-server-cloudimg-amd64-
 #         virt-customize -a [DOWNLOADED_FILE_NAME] --run-command 'apt-get update'
 #         virt-customize -a [DOWNLOADED_FILE_NAME] --mkdir [folder_name]
 #         virt-customize -a [DOWNLOADED_FILE_NAME] --upload [src-file:dest-file]
-
-
-sudo virt-customize -a xenial-server-cloudimg-amd64-disk1.img \
+#Run with sudo
+virt-customize -a xenial-server-cloudimg-amd64-disk1.img \
       --run-command 'apt-get update' \
       --run-command 'DEBIAN_FRONTEND=noninteractive' \
       --run-command 'apt-get install -y \
-                      software-properties-common \
                       net-tools \
                       iproute2 \
                       inetutils-ping \
                       iptables \
-                      aptitude \
-                      wget \
+                      squid3 \
                       curl \
-                      iperf3 \
-                  		iperf \
-                      siege \
-                  		httperf \
-                      hping3 \
-                  		polygraph \
-                      apache2-utils \
                       python \
                       python-yaml \
                       python3 \
                       python3-pip' \
-      --run-command 'cd /home/ubuntu' \
+      --run-command 'useradd -s /bin/bash -d /home/ubuntu -m ubuntu' \
+      --run-command 'cd /home/ubuntu/' \
+      --upload squid.conf:/etc/squid/squid.conf \
       --upload start.sh:/start.sh \
       --upload stop.sh:/stop.sh \
       --run-command 'chown ubuntu start.sh' \
@@ -47,5 +41,5 @@ sudo virt-customize -a xenial-server-cloudimg-amd64-disk1.img \
       --run-command 'chmod +x stop.sh' \
       --mkdir /tngbench_share
 
-#OpenStack command
-openstack image create --public --disk-format qcow2 --container-format bare --file xenial-server-cloudimg-amd64-disk1.img apache-bench-ubuntu
+# OpenStack command to add squid image
+#openstack image create --public --disk-format qcow2 --container-format bare --file xenial-server-cloudimg-amd64-disk1.img squid-vm
