dda6c7a781bf15d2046ba183a5e7e37b082e6193
aahad91 <abdulahadayaz@hotmail.com>
2019-12-09 10:37:04 +0100

    added apache-bench script

--- image-descriptors/apache-bench/start.sh
+++ image-descriptors/apache-bench/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 date > /tngbench_share/start.txt
-
 echo "Apache Bench started ..."
+ab -c 9999 -r -t 60 -n 99999999 -e /tngbench_share/ab_logs2.csv -s 60 -k -X $0:3128 https://www.google.com/
