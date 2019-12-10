48e3394914b69a5d70782d6b04206097502f8e12
aahad91 <abdulahadayaz@hotmail.com>
2019-12-10 19:47:48 +0100

    fix: permission denied error on access.log

--- image-descriptors/squid/stop.sh
+++ image-descriptors/squid/stop.sh
@@ -4,4 +4,5 @@ pkill squid
 sleep 2
 python ./log_intf_statistics.py /tngbench_share/result.yml
 cp /var/log/squid/access.log /tngbench_share/
+chown ubuntu /tngbench_share/access.log
 date > /tngbench_share/stop.txt
