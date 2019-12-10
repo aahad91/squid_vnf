3f86ff7ac4588159d81f796b7d2fbb1594ecbd01
aahad91 <abdulahadayaz@hotmail.com>
2019-12-10 19:29:42 +0100

    added result collection scripts

--- image-descriptors/apache-bench/stop.sh
+++ image-descriptors/apache-bench/stop.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+
+sleep 1
+python ./log_intf_statistics.py /tngbench_share/result.yml
+sleep 1
+python ./process_ab_results.py /tngbench_share/cmd_start.log /tngbench_share/ab_logs2.csv /tngbench_share/result.yml
 date > /tngbench_share/stop.txt
