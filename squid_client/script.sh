acffd0ae4d9c61b9a612cd44af9daef3819a7ecf
aahad91 <abdulahadayaz@hotmail.com>
2020-03-05 21:11:48 +0100

    modified docker compose file and added travis file

--- squid_client/script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-curl -x 172.20.0.2:3128 -L www.google.com  &&
-ab -n 5000 -c 10 -X 172.20.0.2:3128 http://www.google.com/ > test1.txt &&
-curl -x 172.20.0.2:3128 -L www.facebook.com  &&
-ab -n 5000 -c 5 -X 172.20.0.2:3128 http://www.facebook.com/ > test2.txt
-exec "$@";
